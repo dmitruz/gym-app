aeeb512760798ea86265c94698b3182dcce60b40
dmitruz <dmitruz970@gmail.com>
2023-02-15 18:48:56 +0200

    Add Links to pages

--- gym-app/src/scenes/navbar/index.tsx
+++ gym-app/src/scenes/navbar/index.tsx
@@ -2,11 +2,18 @@
 import { useState } from "react";
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
+import Link from './Link';
+import { SelectedPage } from "@/shared/types";
 
 
-type Props = {};
+type Props = {
+    
+    selectedPage: SelectedPage;
+    setSelectedPage: (value: SelectedPage) => void;
 
-const Navbar = (props: Props) => {
+};
+
+const Navbar = ({ selectedPage, setSelectedPage}: Props) => {
 const flexBetween = "flex items-center justify-between";
   return (
     <nav>
@@ -19,10 +26,22 @@ const flexBetween = "flex items-center justify-between";
                 {/* RIGHT SIDE */}
                 <div className={`${flexBetween} w-full`}>
                     <div className={`${flexBetween} gap-8 text-sm`}>
-                        <p>Home</p>
-                        <p>Benefits</p>
-                        <p>Our Classes</p>
-                        <p>Contact Us</p>
+                        <Link page="Home" 
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                        />
+                        <Link page="Benefits"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                        />
+                        <Link page="Our Clases"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                        />
+                        <Link page="Contact Us"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                        />
                     </div>
                     <div className={`${flexBetween} gap-8`}>
                         <p>Sign In</p>
