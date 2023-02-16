63372fbb7ae13f15039131c78f79dbef05b31b8f
dmitruz <dmitruz970@gmail.com>
2023-02-16 21:06:00 +0200

    make mobile menu icons

--- gym-app/src/App.tsx
+++ gym-app/src/App.tsx
@@ -5,9 +5,11 @@ import { SelectedPage } from "./shared/types";
 function App() {
   
 const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
   return (
     <div className="app bg-gray-20">
       <Navbar 
+      isTopOfPage={isTopOfPage}
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}
       />
