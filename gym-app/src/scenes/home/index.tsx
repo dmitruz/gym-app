376e87cbff025f4125774e2094077d6d7d19c7a3
dmitruz <dmitruz970@gmail.com>
2023-02-17 20:44:07 +0200

    image and main header doe

--- gym-app/src/scenes/home/index.tsx
+++ gym-app/src/scenes/home/index.tsx
@@ -6,14 +6,49 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SposorRedbull from "@/assets/SponsorRedbull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Home = ({ setSelectedPage } : Props) => {
+cost isAboveMediumScreens = useMediaQuery("(mi-width:1060px)")
   return (
-    <div>Home</div>
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+         {/* IMAGE AND MAIN HEADER */}
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
+        {/* MAIN HEADER */}
+        <div className="z-10 mt-32 md:basis-3/5">
+          {/* HEADINGS */}
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
+                <img alt="home-page-text" src={HomePageText} />
+              </div>
+            </div>
+
+            <p className="mt-8 text-sm">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </motion.div>
+   
+    </section>
   )
 }
 
