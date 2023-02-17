d0c34d651859f848888f275118c9e5f79cdd0072
dmitruz <dmitruz970@gmail.com>
2023-02-17 20:47:16 +0200

    Style: Header images

--- gym-app/src/scenes/home/index.tsx
+++ gym-app/src/scenes/home/index.tsx
@@ -13,17 +13,17 @@ type Props = {
 };
 
 const Home = ({ setSelectedPage } : Props) => {
-cost isAboveMediumScreens = useMediaQuery("(mi-width:1060px)")
+const isAboveMediumScreens = useMediaQuery("(mi-width:1060px)")
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
-         {/* IMAGE AND MAIN HEADER */}
+    {/* IMAGE AND MAIN HEADER */}
       <motion.div
         className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
         onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
       >
-        {/* MAIN HEADER */}
+    {/* MAIN HEADER */}
         <div className="z-10 mt-32 md:basis-3/5">
-          {/* HEADINGS */}
+    {/* HEADINGS */}
           <motion.div
             className="md:-mt-20"
             initial="hidden"
@@ -47,7 +47,39 @@ cost isAboveMediumScreens = useMediaQuery("(mi-width:1060px)")
               Body Now.
             </p>
           </motion.div>
-   
+
+      {/* ACTIONS */}
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </motion.div>
+        </div>
+        {/* IMAGE */}
+        <div
+          className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end"
+        >
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
+        </div>
+      </motion.div>
     </section>
   )
 }
