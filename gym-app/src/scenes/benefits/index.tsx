701fb51f0948bae80bb57ff61b123c3022ec7561
dmitruz <dmitruz970@gmail.com>
2023-02-17 21:37:24 +0200

    add Benefit Header

--- gym-app/src/scenes/benefits/index.tsx
+++ gym-app/src/scenes/benefits/index.tsx
@@ -7,6 +7,7 @@ import {
 import { motion } from "framer-motion";
 import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
 import Bebefit from "./Benefit";
+import HText from "@/shared/HText";
 
 const benefits: Array<BenefitType> = [
   {
@@ -29,14 +30,46 @@ const benefits: Array<BenefitType> = [
   },
 ];
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
+
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 }
 
 const Benefits = ({setSelectedPage}: Props) => {
-  return 
-    <div>Benefits</div>
-  
+  return (
+     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+ {/* HEADER */}
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>MORE THAN JUST GYM.</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </motion.div>
+      </motion.div>
+      </section>
+  )
 };
 
 export default Benefits;
