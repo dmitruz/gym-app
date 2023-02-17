6f6d29033cdcd034b39b94d68320ddb32934e5c9
dmitruz <dmitruz970@gmail.com>
2023-02-17 20:52:30 +0200

    animation: sponsors.header

--- gym-app/src/scenes/home/index.tsx
+++ gym-app/src/scenes/home/index.tsx
@@ -3,10 +3,11 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
 import HomePageText from "@/assets/HomePageText.png";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
-import SposorRedbull from "@/assets/SponsorRedbull.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -80,6 +81,18 @@ const isAboveMediumScreens = useMediaQuery("(mi-width:1060px)")
           <img alt="home-pageGraphic" src={HomePageGraphic} />
         </div>
       </motion.div>
+    {/* SPONSORS */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   )
 }
