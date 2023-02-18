1c5666aed9798e9de05e33bd1fd438428dd8e075
dmitruz <dmitruz970@gmail.com>
2023-02-18 21:50:35 +0200

    contactUs header

--- gym-app/src/App.tsx
+++ gym-app/src/App.tsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 import { SelectedPage } from "./shared/types";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
-import OurClasses from "./scenes/ourClasses";
+import OurClasses from "@/scenes/ourClasses";
+import ContactUs from "@/scenes/contactUs";
 
 function App() {
   
@@ -31,6 +32,7 @@ useEffect(() => {
       <Home setSelectedPage={setSelectedPage}/>
       <Benefits setSelectedPage={setSelectedPage}/>
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </div>
   )
 }
