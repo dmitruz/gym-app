54632c09dba55c15d247768d5658ea77c60fe73f
dmitruz <dmitruz970@gmail.com>
2023-02-18 21:41:55 +0200

    Done ourClasses component

--- gym-app/src/App.tsx
+++ gym-app/src/App.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { SelectedPage } from "./shared/types";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
+import OurClasses from "./scenes/ourClasses";
 
 function App() {
   
@@ -28,7 +29,8 @@ useEffect(() => {
       setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage}/>
-      <Benefits selectedPage={setSelectedPage}/>
+      <Benefits setSelectedPage={setSelectedPage}/>
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   )
 }
