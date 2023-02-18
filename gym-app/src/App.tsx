462cc03cd189d912ca1262f08132a53ef20fa1c5
dmitruz <dmitruz970@gmail.com>
2023-02-18 22:00:55 +0200

    footer done

--- gym-app/src/App.tsx
+++ gym-app/src/App.tsx
@@ -5,6 +5,7 @@ import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
 import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 
 function App() {
   
@@ -33,6 +34,7 @@ useEffect(() => {
       <Benefits setSelectedPage={setSelectedPage}/>
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   )
 }
